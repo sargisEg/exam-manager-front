4e606fd82d85190243515d5f2a0cff06021b3f39
sargisyeghoyan <sargis.yeghoyan@ihost.am>
2025-03-06 11:47:23 +0400

    Minor changes

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { setupAuth } from "./auth";
+import {createServer} from "http";
 
 const app = express();
 app.use(express.json());
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  const server = registerRoutes(app);
+  const server = createServer(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
